b670da3abecdc4d0de7333ce3a5a6203438f0efa
saito314 <saitou228@gmail.com>
2025-05-27 09:27:45 +0000

    Squareコンポーネントに状態管理を追加し、クリック時に値を更新する機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default function Square() {
-  return <button className="square">X</button>
+
+function Square() {
+  const [value, setValue] = useState<string | null>(null);
+  
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  )
+}
+
+export default function board() {
+  return (
+    <>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
 
