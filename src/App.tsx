8fc25d607f23e0e9dd53b0b2a096c6b579707cf7
saito314 <saitou228@gmail.com>
2025-06-11 10:07:36 +0000

    ゲームの履歴機能を追加し、過去の手を選択できるボタンを実装

--- src/App.tsx
+++ src/App.tsx
@@ -114,13 +114,31 @@ export default function Game() {
     setXIsNext(!xIsNext); // 次のプレイヤーを切り替える
   }
 
+  function jumpTo(nextMove: number) {
+
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
